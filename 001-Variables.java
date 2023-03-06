7efc771ea37795551c6b6ce9f76531a32897b29a
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-06 11:40:07 -0300

    feat: variables

---
+++ 001-Variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        
+        int x = 123;
+        double y = 3.14;
+        float w = 3.14f;
+        boolean z = false;
+        char symbol = '@';
+        String name = "Michael";
+        
+        System.out.println("Int: " + x);
+        System.out.println("Double: " + y);
+        System.out.println("Float: " + w);
+        System.out.println("Boolean: " + z);
+        System.out.println("Char: " + symbol);
+        System.out.println("String: " + name);
+    }
+}
