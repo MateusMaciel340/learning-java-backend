f91d2e4f618dc4700d1b3ebc3c2f0e7805c61dfb
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-07 13:06:46 -0300

    feat: random numbers

---
+++ 007-RandomNumbers.java
@@ -0,0 +1,19 @@
+package randomnumbers;
+
+import java.util.Random;
+
+public class RandomNumbers {
+    public static void main(String[] args) {
+        
+        // used to generate random numbers
+        Random random = new Random();
+        
+        int x = random.nextInt(6) + 1;
+        double y = random.nextDouble();
+        boolean z = random.nextBoolean();
+        
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
