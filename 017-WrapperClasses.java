ad51e0424e0230c862ecaebceb11fdb2b63ebbd9
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-09 10:16:30 -0300

    feat: wrapper classes

---
+++ 017-WrapperClasses.java
@@ -0,0 +1,18 @@
+package wrapperclasses;
+
+public class WrapperClasses {
+    public static void main(String[] args) {
+        
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Mateus";
+        
+        System.out.println("Boolean: " + a);
+        System.out.println("Character: " + b);
+        System.out.println("Integer: " + c);
+        System.out.println("Double: " + d);
+        System.out.println("String: " + e);
+    } 
+}
