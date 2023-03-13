1323112babacc4158621e472ab52c7e11977c26c
mateusmaciel340 <mateusmaciel340@gmail.com>
2023-03-13 16:06:21 -0300

    feat: final keyword

---
+++ stage001/023-FinalKeyword.java
@@ -0,0 +1,11 @@
+package finalkeyword;
+
+public class FinalKeyword {
+    public static void main(String[] args) {
+        
+        final double PI = 3.14159;
+        // PI = 4;
+        
+        System.out.println(PI);
+    }
+}
